a2967f7eb3c2824feecd524fcc88e474f816e612
wushan2025-3 <wushan2025_3@126.com>
2026-05-27 13:53:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 export default {
-  async fetch(request, context) {
-    const { env } = await import("alibaba:workers");
+  async fetch(request, context, env) {
     const TEST_KEY_PLAIN = env.TEST_KEY_PLAIN;
     const TEST_KEY_SECRET = env.TEST_KEY_SECRET;
-    
-    return new Response(`Get TEST_KEY_PLAIN_main_B: ${TEST_KEY_PLAIN} and TEST_KEY_SECRET_main_B: ${TEST_KEY_SECRET} successfully!`);
+
+    return new Response(`Get TEST_KEY_PLAIN_main: ${TEST_KEY_PLAIN} and TEST_KEY_SECRET_main: ${TEST_KEY_SECRET} successfully!`);
   },
 };

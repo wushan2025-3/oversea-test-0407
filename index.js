ec1be3095c941c7b4e2fbb111b2cd3099e1ffee5
wushan2025-3 <wushan2025_3@126.com>
2026-05-27 09:41:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const TEST_KEY_SECRET = env.TEST_KEY_SECRET;
 
 export default {
   async fetch(request, context) {
-    return new Response(`Get TEST_KEY_PLAIN_B: ${TEST_KEY_PLAIN} and TEST_KEY_SECRET_B: ${TEST_KEY_SECRET} successfully!`);
+    return new Response(`Get TEST_KEY_PLAIN_ly_A: ${TEST_KEY_PLAIN} and TEST_KEY_SECRET_ly_A: ${TEST_KEY_SECRET} successfully!`);
   },
 };
